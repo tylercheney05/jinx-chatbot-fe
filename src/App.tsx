b7dc1abe4ee614d765b6bfee4f808c00628a822e
Tyler Cheney <tyler.cheney.05@gmail.com>
2025-11-18 23:27:35 -0800

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import "./index.css"
+import Chatbot from "./containers/Chatbot"
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen m-12">
+      <Chatbot />
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/Chatbot.tsx
@@ -0,0 +1,62 @@
+import { Input } from "@/components/ui/input"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import LimeBoiPNG from "@/assets/lime-boi.png"
+import { Button } from "@/components/ui/button"
+import { Forward, Loader } from "lucide-react"
+import { useState } from "react"
+import axios from "axios"
+import { Separator } from "../components/ui/separator"
+
+const ChatbotCard = () => {
+  const [loading, setLoading] = useState<boolean>(false)
+  const [response, setResponse] = useState<string>("")
+  const [value, setValue] = useState<string>("")
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)
+
+  const handleSubmit = async () => {
+    setLoading(true)
+    const response = await axios.post(`${import.meta.env.VITE_API_HOST}/chatbot`, {
+      question: value,
+    })
+    setResponse(response.data)
+    setLoading(false)
+  }
+
+  return (
+    <div className="flex gap-4 flex-col">
+      <Card className="max-w-sm">
+        <CardHeader>
+          <img src={LimeBoiPNG} alt="LimeBoi" className="w-60 mx-auto" />
+          <CardTitle className="text-center">Hi! My name is Louie the Lime Boi!</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form
+            className="flex gap-2 mb-4"
+            onSubmit={(e) => {
+              e.preventDefault()
+              handleSubmit()
+            }}
+          >
+            <Input type="text" value={value} onChange={onChange} placeholder="Ask me anything..." />
+            <Button type="submit" onClick={handleSubmit}>
+              <Forward />
+            </Button>
+          </form>
+          {loading && <Loader className="animate-spin mx-auto" />}
+          {!loading && response && (
+            <>
+              <Separator className="my-8" />
+              <p className="whitespace-pre-wrap">
+                <strong>Lime Boi: </strong>
+                {response}
+              </p>
+            </>
+          )}
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default ChatbotCard
